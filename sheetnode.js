3b4b5b1a994dd05077d597ee4e65753590469674
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2008-11-11 07:28:52 +0000

    CSS adjustments

--- sheetnode.js
+++ sheetnode.js
@@ -5,6 +5,8 @@ Drupal.sheetnode = {};
 Drupal.sheetnode.startUp = function() {
   SocialCalc.Constants.defaultImagePrefix = Drupal.settings.sheetnode.imageprefix;
   this.sheet = new SocialCalc.SpreadsheetControl();
+  this.sheet.tabbackground="display:none;";
+  this.sheet.toolbarbackground="display:none;";
   this.sheet.ParseSheetSave(Drupal.settings.sheetnode.value);
   this.sheet.InitializeSpreadsheetControl(Drupal.settings.sheetnode.element);
 }
