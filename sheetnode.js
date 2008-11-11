7834d2a835530fe5110aeb54f609e4d0bdf8972e
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2008-11-11 00:27:02 +0000

    Adjusted for socialcalc SVN trunk

--- sheetnode.js
+++ sheetnode.js
@@ -2,28 +2,16 @@
 
 Drupal.sheetnode = {};
 
-Drupal.sheetnode.startUp = function(str, ele) { 
-  this.sheet = new SocialCalc.Sheet();
-  this.sheet.ParseSheetSave(str);
-  this.context = new SocialCalc.RenderContext(this.sheet);
-  this.context.showGrid = true;
-  this.context.showRCHeaders = true;
-  this.context.CalculateCellSkipData();
-  this.editor = new SocialCalc.TableEditor(this.context);
-  this.editor.imageprefix = Drupal.settings.sheetnode.imageprefix;
-  this.sizes = SocialCalc.GetViewportInfo();
-  this.teelement = this.editor.CreateTableEditor(this.sizes.width-20, this.sizes.height-165);
-  ele.replaceChild(this.teelement, ele.firstChild);
-  this.editor.SchedulePositionCalculations();
-}
-
-Drupal.sheetnode.save = function(ele) {
-  ele.value = this.sheet.CreateSheetSave();
+Drupal.sheetnode.startUp = function() {
+  SocialCalc.Constants.defaultImagePrefix = Drupal.settings.sheetnode.imageprefix;
+  this.sheet = new SocialCalc.SpreadsheetControl();
+  this.sheet.ParseSheetSave(Drupal.settings.sheetnode.value);
+  this.sheet.InitializeSpreadsheetControl(Drupal.settings.sheetnode.element);
 }
 
 $(document).ready(function() {
   $('#edit-submit').click(function() {
-    Drupal.sheetnode.save($('#edit-save')[0]);
+    $('#edit-save').val(Drupal.sheetnode.sheet.CreateSheetSave());
   });
 });
 
