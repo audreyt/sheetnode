9cf6971559dba0d6b88430231d7dc833a873c970
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2008-11-11 21:56:34 +0000

    Added window resizing adjustment

--- sheetnode.js
+++ sheetnode.js
@@ -11,9 +11,19 @@ Drupal.sheetnode.startUp = function() {
   this.sheet.InitializeSpreadsheetControl(Drupal.settings.sheetnode.element);
 }
 
+Drupal.sheetnode.resize = function() {
+  if (this.sheet.SizeSSDiv()) {
+    this.sheet.editor.ResizeTableEditor(this.sheet.width, this.sheet.height-
+      (this.sheet.spreadsheetDiv.firstChild.offsetHeight + this.sheet.formulabarDiv.offsetHeight));
+  }
+}
+
 $(document).ready(function() {
   $('#edit-submit').click(function() {
     $('#edit-save').val(Drupal.sheetnode.sheet.CreateSheetSave());
+  });
+  $(window).resize(function() {
+    Drupal.sheetnode.resize();
   });
 });
 
