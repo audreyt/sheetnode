3983605c35084b0004d55cc902df882d37a2c6e3
Karim Ratib <karim.ratib@gmail.com>
2012-05-10 20:52:36 -0700

    Support refering to other sheets on the same page #1378070

--- sheetnode.js
+++ sheetnode.js
@@ -111,7 +111,7 @@ Drupal.sheetnode.prototype.loadsheetSetup = function() {
     var found = false;
 
     // Avoid returning this sheet.
-    if (self.settings.name == sheetname) {
+    if (self.settings.aliases.indexOf(sheetname) != -1) {
       // TODO: Should really return true and inform SocialCalc that this is the same sheet.
       // However SocialCalc does not support aliases currently.
       return false;
@@ -119,7 +119,7 @@ Drupal.sheetnode.prototype.loadsheetSetup = function() {
 
     // First examine on-screen spreadsheets.
     $.each(Drupal.sheetnode.sheetviews, function(index, sheetnode) {
-      if (sheetnode.settings.name == sheetname) {
+      if (sheetnode.settings.aliases.indexOf(sheetname) != -1) {
         var data = sheetnode.spreadsheet.CreateSpreadsheetSave();
         var parts = sheetnode.spreadsheet.DecodeSpreadsheetSave(data);
         if (parts.sheet) {
@@ -382,4 +382,38 @@ parseUri.options = {
     loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
   }
 };
+
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf#Compatibility
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+    "use strict";
+    if (this == null) {
+      throw new TypeError();
+    }
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (len === 0) {
+      return -1;
+    }
+    var n = 0;
+    if (arguments.length > 0) {
+      n = Number(arguments[1]);
+      if (n != n) { // shortcut for verifying if it's NaN
+          n = 0;
+      } else if (n != 0 && n != Infinity && n != -Infinity) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+    }
+    if (n >= len) {
+      return -1;
+    }
+    var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+    for (; k < len; k++) {
+      if (k in t && t[k] === searchElement) {
+        return k;
+      }
+    }
+    return -1;
+  }
+}
 
